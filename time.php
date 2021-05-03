c724912e8cdb95149354154bd29f1d8c0745ba20
yatharth-vataliya <yatharthvataliya@gmail.com>
2021-05-03 10:05:09 +0530

    some changes for perfomance testing in time.php file

--- time.php
+++ time.php
@@ -3,7 +3,10 @@
 $t1 = microtime(true);
 
 
-for($i = 0; $i < 100000; $i++){
+for ($i = 0; $i < 10000000; $i++) {
+    $file = fopen('time.txt', 'a+');
+    fwrite($file, 'This is time test with file write');
+    fclose($file);
     echo "$i \n";
 }
 
@@ -12,5 +15,3 @@ $t2 = microtime(true);
 $t3 = $t2 - $t1;
 
 echo $t3;
-
-?>
