484f696a010e6fca1f61f094a60ef09c35f3ddef
yatharth-vataliya <yatharthvataliya@gmail.com>
2022-11-01 20:25:32 +0530

    minor correction

--- header_test.php
+++ header_test.php
@@ -6,12 +6,12 @@ return header('location:js_send.php'); // but if we add the return statement wit
 
 exit; // manually exit the script
 
+header('location: js_send.php'); // this will redirect to js_send.php but execute remaining script, we can stop execution by return keyword before header function or manually cally exit or die function of php
+
 $handle = fopen('demo.txt','a+');
 
 fwrite($handle, 'nothing');
 
 fclose($handle);
-
-header('location:mongodb.php'); // if script is not terminated manually then this header redirection will execute and above once or all is ignored.
 
 ?>
