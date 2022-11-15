6a35a30dba6f13b887075022add0eacd09131129
yatharth-vataliya <yatharthvataliya@gmail.com>
2022-11-15 15:49:45 +0530

    Attribute example of php 8.1

---
+++ php_8_1/Attribute/AttributeExample.php
@@ -0,0 +1,9 @@
+<?php
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class AttributeExample
+{
+    public function __construct(?string $class = null){
+        echo "This is class ". $class . "\r\n";
+    }
+}
