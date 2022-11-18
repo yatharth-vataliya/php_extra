0bb08cb6d144db5030b05ba3d2107f7650edcb33
yatharth-vataliya <yatharthvataliya@gmail.com>
2022-11-18 23:40:10 +0530

    PureInteractionType and NestedAttribute feature of php8.1 is explained

---
+++ php_8_1/PureInteractionType.php
@@ -0,0 +1,25 @@
+<?php
+
+#PHP < 8.1
+function countAndIterate(Iterator $value): void
+{
+    if (!($value instanceof Countable)) {
+        throw new TypeError('value must be Countable');
+    }
+
+    foreach ($value as $val) {
+        echo $val;
+    }
+
+   echo count($value);
+}
+
+#PHP 8.1
+function countAndIterate2(Iterator&Countable $value): void
+{
+    foreach ($value as $val) {
+        echo $val;
+    }
+
+    echo count($value);
+}
