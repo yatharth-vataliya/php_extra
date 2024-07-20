5a1eba5af858eea8f07a1b4c9d2003f0e914c672
yatharth-vataliya <yatharthvataliya@gmail.com>
2024-07-20 22:11:19 +0530

    some more example added to switch_case file

--- switch_case.php
+++ switch_case.php
@@ -3,12 +3,12 @@
 $i = 0;
 
 switch ($i) {
-  case 0:
-    echo " 0 is printed";
-  case 1:
-    echo " 1 is printed";
-  case 2:
-    echo " 2 is printed";
+    case 0:
+        echo ' 0 is printed';
+    case 1:
+        echo ' 1 is printed';
+    case 2:
+        echo ' 2 is printed';
 }
 
 echo "\n";
@@ -24,13 +24,13 @@ it will execute all the case because there is not break statement
 */
 
 switch ($i) {
-  case 0:
-    echo " 0 is printed";
-  case 1:
-    echo " 1 is printed";
-    break;
-  case 2:
-    echo " 2 is printed";
+    case 0:
+        echo ' 0 is printed';
+    case 1:
+        echo ' 1 is printed';
+        break;
+    case 2:
+        echo ' 2 is printed';
 }
 
 echo "\n";
@@ -46,16 +46,16 @@ it will execute the all the cases unitl the break statement is executed
 */
 
 switch (2) {
-  case 0:
-    echo " 0 is printed";
-  case 1:
-    echo " 1 is printed";
-    break;
-  case 2:
-    echo " 2 is printed";
-  default:
-    echo " this is default statement";
-    break 1; //see the php manual for the numberic syntax
+    case 0:
+        echo ' 0 is printed';
+    case 1:
+        echo ' 1 is printed';
+        break;
+    case 2:
+        echo ' 2 is printed';
+    default:
+        echo ' this is default statement';
+        break; //see the php manual for the numberic syntax
 }
 
 echo "\n";
@@ -72,16 +72,36 @@ in this state there is no break statement after the matching case so default sta
 
 $beer = 'carlsberg';
 
-switch($beer)
-{
-    case 'tuborg';
-    case 'carlsberg';
-    case 'heineken';
+switch ($beer) {
+    case 'tuborg':
+    case 'carlsberg':
+    case 'heineken':
         echo 'Good choice';
-    break;
-    default;
+        break;
+    default:
         echo 'Please make a new selection...';
-    break;
+        break;
 }
 // it is possible to use semicolon as ':'
-?>
+
+echo "\n";
+
+$i = 1;
+
+switch ($i) {
+    case 1:
+        echo 'one printed';
+        echo "\n";
+    case 2:
+        echo 'two printed';
+        echo "\n";
+    case 3:
+        echo 'three printed';
+        echo "\n";
+    case 4:
+        echo 'four printed';
+        echo "\n";
+    default:
+        echo 'default printed';
+        echo "\n";
+}
