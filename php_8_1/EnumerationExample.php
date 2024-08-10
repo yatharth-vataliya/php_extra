48bc7ec3bc7c419902048b4ce235792dfdbb5868
yatharth-vataliya <yatharthvataliya@gmail.com>
2024-08-10 12:03:11 +0530

    some more stuff added to EnumerationExample php file

--- php_8_1/EnumerationExample.php
+++ php_8_1/EnumerationExample.php
@@ -1,13 +1,13 @@
 <?php
 
-/* 
+/*
 enum Suit : string {
     case Hearts;
     case Diamonds;
     case Clubs;
     case Spades;
 }
-*/  // This enum style is also valid. This style didn't contain value for it's case
+*/ // This enum style is also valid. This style didn't contain value for it's case
 
 // We can't give value to one enum case and left others, That is not valid. So There is enum where all case have value or no value in any case
 
@@ -18,29 +18,47 @@ enum Suit : string {
 
 //enum that contains values might have type declaration other wise it will throw exception
 
-enum Suit : string {
-    case Hearts = "Red";
-    case Diamonds = "Black";
-    case Clubs = "Green";
-    case Spades = "Yellow";
+enum Suit: string
+{
+    case Hearts = 'Red';
+    case Diamonds = 'Black';
+    case Clubs = 'Green';
+    case Spades = 'Yellow';
 }
 
 $suit = Suit::Hearts;  // Get enum case named Hearts;
 
 echo $suit->name; // print the name of enum case
 
-echo "<br/>". PHP_EOL;
+echo '<br/>'.PHP_EOL;
 
 echo $suit->value; // print the value of enum case if it enum case has value
 
-echo "<br/>". PHP_EOL;
+echo '<br/>'.PHP_EOL;
 
-$suit = Suit::from("Yellow"); // This will Find out case that have value Yellow and give object for of matched enum case. This will throw error when not found matching value.
+$suit = Suit::from('Yellow'); // This will Find out case that have value Yellow and give object for of matched enum case. This will throw error when not found matching value.
 
-echo "<br/>". PHP_EOL;
+echo '<br/>'.PHP_EOL;
 
-$suit = Suit::tryFrom("Pink"); // This tryFrom will give null if not found any matching enum case
+$suit = Suit::tryFrom('Pink'); // This tryFrom will give null if not found any matching enum case
 
 var_dump($suit);
 
 var_dump(Suit::cases()); // EnumClass::cases();  cases() method will list out all of the cases of enum classes
+
+var_dump(Suit::Diamonds);
+
+var_dump(Suit::Diamonds->value);
+
+var_dump(Suit::class);
+
+var_dump(Suit::Diamonds->cases());
+
+var_dump(Suit::Diamonds->tryFrom('Red'));
+
+var_dump(Suit::Diamonds->from('Green'));
+
+var_dump(Suit::Diamonds->tryFrom('Nothing'));
+
+// var_dump(Suit::Diamonds->from('Nothing')); // This will give error
+
