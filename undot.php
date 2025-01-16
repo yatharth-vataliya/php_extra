3b8506c302e32437fe9a76c85ae517a3bd44de4d
yatharth-vataliya <yatharthvataliya@gmail.com>
2025-01-16 20:01:56 +0530

    dot undot or you can say dataSet and dataGet function similar to laravel data_set and data_get implemented but not fully identical its only basic implementation

---
+++ undot.php
@@ -0,0 +1,52 @@
+<?php
+
+function dataSet(&$target, string|array $dotString, $value, ?bool $overwrite = true)
+{
+    $target = $target ?? [];
+    $dotVarArray = is_array($dotString) ? $dotString : (explode('.', $dotString) ?? []);
+    $firstPoint = array_shift($dotVarArray);
+    if ($dotVarArray) {
+        if (!array_key_exists($firstPoint, $target)) {
+            $target[$firstPoint] = [];
+        }
+        dataSet($target[$firstPoint], $dotVarArray, $value, $overwrite);
+    } elseif ($overwrite) {
+        $target[$firstPoint] = $value;
+    }
+}
+
+function dataGet(string $dotString, array $mixedArray): mixed
+{
+    if (count($mixedArray) > 0 && $dotString) {
+
+        $returnValue = null;
+        foreach (explode('.', $dotString) as $dot) {
+            if (!array_key_exists($dot, $mixedArray)) {
+                $returnValue = null;
+                break;
+            }
+            $returnValue = $returnValue ? $returnValue[$dot] : $mixedArray[$dot];
+        }
+
+        return $returnValue;
+    }
+    return null;
+}
+
+$dotString = 'userinfo.name.first_name';
+
+dataSet($data, $dotString, 'Yatharth', false);
+
+var_dump($data);
+
+var_dump(dataGet($dotString, $data ?? []));
+
+// both array declarations are identical
+$data['userinfo']['name']['first_name'] = 'Yatharth';
+$data = array(
+    'userinfo' => [
+        'name' => [
+            'first_name' => 'Yatharth',
+        ],
+    ],
+);
